69d3c6fe0eb9c2938ccfdee82e3adb8327e15f54
Luis Sarmiento <luis@MacBook-Pro-de-Luis.local>
2024-07-10 17:28:06 -0500

    Version 1.10

--- dash.php
+++ dash.php
@@ -2,42 +2,26 @@
  
 <?php
  session_start();
- 
-                        $_SESSION['user_id'] = 1; //$result_row->user_id;
-		            	$_SESSION['user_name'] = "ADMIN.SISTEMA"; // $result_row->user_name;
-                        $_SESSION['user_email'] = "admin@admin.co"; //$result_row->user_email;
-                        $_SESSION['user_perfil'] = 1; //$result_row->perfil;
-                        $_SESSION['user_firstname'] = "Usuario"; //$result_row->firstname;
-                        $_SESSION['user_lastname'] = "Administrador"; //$result_row->lastname;  
-                        
-                        $_SESSION['idUt'] = 99; //$result_row->idUt;
-                        $_SESSION['nombreUt'] = "UNP"; //$result_row->nombreUt;  
-                        
-                        $_SESSION['user_login_status'] = 1;
-                        
-                        if ($_SESSION['user_perfil']==1)
-                        {
-                            $_SESSION['nombre_perfil']="ADMINISTRADOR";
-                        }
-                        if ($_SESSION['user_perfil']==3)
-                        {
-                            $_SESSION['nombre_perfil']="JURIDICO";
-                        }
-                        if ($_SESSION['user_perfil']==4)
-                        {
-                            $_SESSION['nombre_perfil']="FINANCIERO";
-                        }
-                        if ($_SESSION['user_perfil']==5)
-                        {
-                            $_SESSION['nombre_perfil']="UT";
-                        }
+
                         
  $nomUusuario = $_SESSION['user_name'];
  $emaiUsuario = $_SESSION['user_email'];
  $nomUsuarioI = $_SESSION['user_firstname'];
  $apeUsuarioI = $_SESSION['user_lastname'];      
- $idBodega    = $_SESSION['laBodega'];    
- $nomBodega   = $_SESSION['laTienda'];     
+ 
+ /*
+ echo "1.." . $nomUusuario ;
+ echo '<br>';
+ echo "2.." . $emaiUsuario;
+ echo '<br>';
+ echo "3.." . $nomUsuarioI;
+ echo '<br>';
+ echo "4.." . $apeUsuarioI;      
+ echo '<br>';
+ echo "5.." . $idBodega;    
+ echo '<br>';
+ echo "6.." . $nomBodega;     
+ */
 
  $nombreUsuario=$nomUsuarioI . " " . $apeUsuarioI;
  
--- index.php
+++ index.php
@@ -1,108 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>UNP Reuniones</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.6.3/css/all.css' integrity='sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/' crossorigin='anonymous'>
+    
+</head>
+<body>
+    
 <?php
-
-
-$s_LA    = $_GET['id_user'];
-echo "Id enviado: " . $s_LA;
+session_start();
+$buscar    = intval($_GET['id_user']);
+echo "Id enviado: " . $buscar;
 echo "<br>";
-include("dash.php");
-                        //header("Location: ini00.php");
-                        $_SESSION['user_id'] = 1; //$result_row->user_id;
-		            	$_SESSION['user_name'] = "ADMIN.SISTEMA"; // $result_row->user_name;
-                        $_SESSION['user_email'] = "admin@admin.co"; //$result_row->user_email;
-                        $_SESSION['user_perfil'] = 1; //$result_row->perfil;
-                        $_SESSION['user_firstname'] = "USUARIO"; //$result_row->firstname;
-                        $_SESSION['user_lastname'] = "ADMINISTRADOR"; //$result_row->lastname;  
-                        
-                        $_SESSION['idUt'] = 99; //$result_row->idUt;
-                        $_SESSION['nombreUt'] = "UNP"; //$result_row->nombreUt;  
-
-                        //$_SESSION['ip_add'] = $_SERVER['REMOTE_ADDR'];
-                        //$laIp=$_SESSION['ip_add'];
-                          
-                        $_SESSION['user_login_status'] = 1;
-                        
-                        if ($_SESSION['user_perfil']==1)
-                        {
-                            $_SESSION['nombre_perfil']="ADMINISTRADOR";
-                        }
-                        if ($_SESSION['user_perfil']==3)
-                        {
-                            $_SESSION['nombre_perfil']="JURIDICO";
-                        }
-                        if ($_SESSION['user_perfil']==4)
-                        {
-                            $_SESSION['nombre_perfil']="FINANCIERO";
-                        }
-                        if ($_SESSION['user_perfil']==5)
-                        {
-                            $_SESSION['nombre_perfil']="UT";
-                        }
 
 
+//http://ecosistemasesp.unp.gov.co/usuarios/api/usuario/
+//URL de la API de lectura
+$url = 'https://softmakr.com/reuniones/config/usuarios.txt';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
 
+// Ejecutar la solicitud y obtener la respuesta
+$response = curl_exec($ch);
 
-
-/*  echo file_get_contents('http://ecosistemasesp.unp.gov.co/usuarios/api/usuario/luis.sarmiento');
-//$data = json_decode( file_get_contents('http://ecosistemasesp.unp.gov.co/usuarios/api/usuario/luis.sarmiento/'), true );
-//echo $data['email'];
-header("Location: http://ecosistemasesp.unp.gov.co/usuarios/api/usuario/username:luis.sarmiento");*/
-
-
-/*
-prueba 1
-   // URL o endpoint de la API 
-    $url = 'https://nubecolectiva.com/api/v1/postres';
- 
-    // Iniciamos CURL en PHP 
-    $curl = curl_init($url);
-    echo "1.." . $curl;
-    
- 
-    // Iniciamos la transferencia de datos con CURL en PHP 
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
- 
-    // Pasamos un encabezado JSON a la peticiÃ³n de los datos 
-    curl_setopt($curl, CURLOPT_HTTPHEADER, [
-        'Content-Type: application/json'
-    ]);
- 
-    // Ejecutamos una sesiÃ³n CURL y solicitamos los datos de la API 
-    $datos = curl_exec($curl);    
-    
-    // Decodificamos los datos recibidos de la API
-    $productos = json_decode($datos, true);
-    echo "3.." . $productos;
-    // Imprimimos los datos en la pÃ¡gina web 
-    print_r($productos);
- 
-    // Cerramos la sesiÃ³n de CURL por seguridad 
-    curl_close($curl);
-
-https://nubecolectiva.com/api/v1/postres/
-http://ecosistemasesp.unp.gov.co/usuarios/api/usuario/
-*/
-
- /*
-$url = 'https://nubecolectiva.com/api/v1/postres/';
-$data = file_get_contents($url);
-echo "data .." . $data;
-
-if ($data === false) {
-// Manejar el error
-echo "no lee";
-} else {
-// Procesar los datos obtenidos de la API
-$jsonData = json_decode($data, true);
-echo '<br>';
-
-echo "data .." . $jsonData;
-echo '<br>';
-var_dump($jsonData);
-
+// Verificar si hubo alg¨²n error en la solicitud
+if(curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
 }
-*/
-//require("dash.php");
 
+// Cerrar la conexi¨®n cURL
+curl_close($ch);
+
+// Decodificar la respuesta JSON
+$data = json_decode($response, true);
+
+// Verificar si la decodificaci¨®n fue exitosa
+if ($data === null) {
+    echo "Error al decodificar el JSON";
+} else {
+    //$buscar = 1;
+    $encontrado = false;
+    //echo '<br>';
+    //echo "buscar.." .$buscar;
+    // Procesar los datos obtenidos
+    foreach ($data as $usuario) {
+        $id = $usuario['id'];
+        if ($id === $buscar) {
+          $encontrado = true;
+          $first_name = $usuario['first_name'];
+          $last_name  = $usuario['last_name'];
+          $email      = $usuario['email'];
+          $username   = $usuario['username'];
+          
+          $_SESSION['user_id']        = $id;
+          $_SESSION['user_name']      = $username;
+          $_SESSION['user_email']     = $email;
+          $_SESSION['user_perfil']    = 1;//forzado admin
+          $_SESSION['user_firstname'] = $first_name;
+          $_SESSION['user_lastname']  = $last_name; 
+          
+          /*
+          echo "1." .$id;
+          echo '<br>';
+          echo "2." .$first_name;
+          echo '<br>';
+          echo "3." .$last_name;
+          echo '<br>';
+          echo "4." .$email;
+          echo '<br>';
+          echo "5." .$username;
+          echo '<br>';
+          */
+          break;  // Detener el bucle una vez que se encuentra el valor
+        }
+    } //foreach
+    
+    if (!$encontrado) {
+       //echo "El valor '$buscar' no fue encontrado en el registro."; 
+?>       
+       <div class="container" align="center">
+          <div class="alert alert-info">
+              <h1><strong>ALERTA!</strong> </h1> 
+              <h2>El Id: <?=$buscar?> </h2> 
+              <h3>No fue encontrado en el registro.</h3>
+              <a href="http://ecosistemasesp.unp.gov.co/" class="btn btn-primary btn-block"><i  class='fas fa-sign-in-alt'> </i> Regresar </a> 
+
+           </div>  
+       </div>
+<?php       
+    }
+    else
+    {
+      include("dash.php");
+    }
+}//else    
 
 
 ?>
+
+
+
+
