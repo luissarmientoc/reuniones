bc4a6de7f1aa37a8fe9be3a15e9dc7103fec31db
Luis Sarmiento <luis@MacBook-Pro-de-Luis.local>
2024-07-17 11:51:58 -0500

    Version 1.30

--- ajax/buscar_entidad.php
+++ ajax/buscar_entidad.php
@@ -37,77 +37,88 @@
         
         if($action == 'ajax')
         {
-    
-        // Configurar el modo de error para excepciones
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sWhere = "";
-		 
-		$sWhere.=" order by nombreentidad";
-		include 'pagination.php'; //include pagination file
-		//pagination variables
-		$page = (isset($_REQUEST['page']) && !empty($_REQUEST['page']))?$_REQUEST['page']:1;
-		$per_page = 10; //how much records you want to show
-		$adjacents  = 4; //gap between pages after number of adjacents
-		$offset = ($page - 1) * $per_page;
-		
-	    // Consulta SQL para contar las filas
-        $sql = "SELECT COUNT(*) AS total_filas FROM $sTable";
-        $stmt = $pdo->query($sql);
-        // Obtener el resultado (única fila)
-        $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
-        // Número total de filas
-        $total_filas = $resultado['total_filas'];
-        $total_pages = ceil($total_filas / $per_page);
-        $reload = './marcas.php';
-        //echo "El número total de filas en la tabla es: $total_filas";
+           
+           // Limpiar y escapar la cadena de texto (strip_tags y htmlentities)
+           $q = strip_tags($_REQUEST['q']);
+           $q = htmlentities($q, ENT_QUOTES, 'UTF-8');
         
-        if ($total_filas>0)
-        {
-    ?>
-            <div class="table-responsive">
-			  <table class='tablaResponsive table table-striped table-bordered table-hover'>
-				<tr  class="info">
-					<th>Entidad</th>
-					<th class='text-center'>Acciones</th>
-				</tr>
-    <?php
-                // Consulta tabla base
-               //$sql="SELECT * FROM " . $sTable ;
+         echo '<br>';
+         echo "la q..." . $q;
+         echo '<br>';
+         
+            
+            
+            
+           // Configurar el modo de error para excepciones
+           $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		   $sWhere = "";
+		 
+		   $sWhere.=" order by nombreentidad";
+		   include 'pagination.php'; //include pagination file
+		   //paginación variables
+		   $page = (isset($_REQUEST['page']) && !empty($_REQUEST['page']))?$_REQUEST['page']:1;
+		   $per_page = 10; //cuantos registros desea mostrar
+		   $adjacents  = 4; //gap entre paginas despues del número de adyacentes
+		   $offset = ($page - 1) * $per_page;
+		
+	       // Consulta SQL para contar las filas
+           $sql = "SELECT COUNT(*) AS total_filas FROM $sTable";
+           $stmt = $pdo->query($sql);
+           // Obtener el resultado (única fila)
+           $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+           // Número total de filas
+           $total_filas = $resultado['total_filas'];
+           $total_pages = ceil($total_filas / $per_page);
+           $reload = './marcas.php';
+           //echo "El número total de filas en la tabla es: $total_filas";
+        
+           if ($total_filas>0)
+           {
+       ?>
+               <div class="table-responsive">
+			     <table class='tablaResponsive table table-striped table-bordered table-hover'>
+				   <tr  class="info">
+				   	   <th>Entidad</th>
+					   <th class='text-center'>Acciones</th>
+				   </tr>
+       <?php
+                   // Consulta tabla base
+                  //$sql="SELECT * FROM " . $sTable ;
                
-               $sql="SELECT * FROM  $sTable $sWhere OFFSET $offset LIMIT $per_page";
-               $stmt = $pdo->query($sql);
-               while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                    $idEntidad=$row['identidad'];
-				    $nombreEntidad=$row['nombreentidad'];
-				    $lv   = $idEntidad. "/MOD1234567890qwertyuiopasdfghjkl";
-				    $lVDX = base64_encode($lv);
+                  $sql="SELECT * FROM  $sTable $sWhere OFFSET $offset LIMIT $per_page";
+                  $stmt = $pdo->query($sql);
+                  while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                       $idEntidad=$row['identidad'];
+				       $nombreEntidad=$row['nombreentidad'];
+				       $lv   = $idEntidad. "/MOD1234567890qwertyuiopasdfghjkl";
+				       $lVDX = base64_encode($lv);
                
-                   //echo "Nombrexxxxxxx: {$row['nombreentidad']}<br />";
-    ?>
-                    <tr>	
-  					   <td><?php echo $nombreEntidad; ?></td>
+                      //echo "Nombrexxxxxxx: {$row['nombreentidad']}<br />";
+       ?>
+                       <tr>	
+  		   			      <td><?php echo $nombreEntidad; ?></td>
 					   
-					   <td class='text-center'>
-					     <a href="entidad1.php?LA=<?=$lVDX?>" class='btn btn-default' title='Editar entidad' ><i class="glyphicon glyphicon-edit"></i></a> 
-					     <a href="#" class='btn btn-default' title='Borrar entidad' onclick="eliminar('<?php echo $idEntidad; ?>')"><i class="glyphicon glyphicon-trash"></i> </a>
-					    </td>
-					</tr>
-    <?php
-            }//while
-    ?>        
-                    <tr>
-					   <td colspan="2">
-					      <span class="pull-right">
-					         <?php
-					           echo paginate($reload, $page, $total_pages, $adjacents);
-					         ?>
-					       </span>
-					   </td>
-				    </tr>
-			  </table>
-	<?php		  
-        }//if
-        }//ajax
+   					      <td class='text-center'>
+					        <a href="entidad1.php?LA=<?=$lVDX?>" class='btn btn-default' title='Editar entidad' ><i class="glyphicon glyphicon-edit"></i></a> 
+					        <a href="#" class='btn btn-default' title='Borrar entidad' onclick="eliminar('<?php echo $idEntidad; ?>')"><i class="glyphicon glyphicon-trash"></i> </a>
+					       </td>
+					   </tr>
+       <?php
+               }//while
+       ?>        
+                       <tr>
+					      <td colspan="2">
+					         <span class="pull-right">
+					            <?php
+					              echo paginate($reload, $page, $total_pages, $adjacents);
+					            ?>
+					          </span>
+					      </td>
+				       </tr>
+			     </table>
+	   <?php		  
+            }//if
+          }//ajax
  	}catch (PDOException $e) {
     echo "Error de conexión: " . $e->getMessage();
 }
