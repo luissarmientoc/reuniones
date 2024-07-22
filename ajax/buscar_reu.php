9619b157a0825e7fdab51d143fad608570b2f135
Luis Sarmiento <luis@MacBook-Pro-de-Luis.local>
2024-07-22 15:41:15 -0500

    Version jul-22

--- ajax/buscar_reu.php
+++ ajax/buscar_reu.php
@@ -5,63 +5,103 @@
 	require_once ("../config/conexion.php");//Contiene funcion que conecta a la base de datos
  	$action = (isset($_REQUEST['action'])&& $_REQUEST['action'] !=NULL)?$_REQUEST['action']:'';
  	
-	if (isset($_GET['id'])){
-		$id_reunion=intval($_GET['id']);
-		$sql="select * from reu_reuniones_participante where idReunion=$id_reunion";
- 		$query=mysqli_query($con, $sql);
-		$count=mysqli_num_rows($query);
-		if ($count==0){
- 		    $borrar="DELETE FROM reu_reuniones  WHERE idReunion='".$id_reunion."'";
-		    //echo $borrar;
-			if ($delete1=mysqli_query($con,$borrar)){
-			?>
-			<div class="alert alert-success alert-dismissible" role="alert">
-			  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-			  <strong>Atención!</strong> Datos eliminados exitosamente.
-			</div>
-			<?php 
-		}else {
-			?>
-			<div class="alert alert-danger alert-dismissible" role="alert">
-			  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-			  <strong>Error!</strong> Lo siento algo ha salido mal intenta nuevamente.
-			</div>
-			<?php
-			
-		}
-			
-		} else {
-			?>
-			<div class="alert alert-danger alert-dismissible" role="alert">
-			  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-			  <strong>Error!</strong> No se pudo eliminar ésta reunión. Existen participantes vinculados a la reunión. 
-			</div>
-			<?php
-		}
-		
-	}
-	if($action == 'ajax'){
-		// escaping, additionally removing everything that could be (html/javascript-) code
-         $q  = mysqli_real_escape_string($con,(strip_tags($_REQUEST['q'], ENT_QUOTES)));
-         $q1 = mysqli_real_escape_string($con,(strip_tags($_REQUEST['q1'], ENT_QUOTES)));
-         $q2 = mysqli_real_escape_string($con,(strip_tags($_REQUEST['q2'], ENT_QUOTES)));
-         $q3 = mysqli_real_escape_string($con,(strip_tags($_REQUEST['q3'], ENT_QUOTES)));
-        /* 
-        echo '<br>';
-        echo "q: " . $q;
-        echo '<br>';
-        echo "q1: " . $q1;
-        echo '<br>';
-        echo "q2: " . $q2;
-        echo '<br>';
-        echo "q3: " . $q3;
-        echo '<br>';
-        echo "q4: " . $q4;
-        echo '<br>';
-        */      
-		 $aColumns = array('idReunion');//Columnas de busqueda
-		 $sTable = "reu_reuniones";
-		 $sWhere = "";
+   	
+ 	try {
+ 	    // Crear una nueva instancia de conexión PDO
+        $pdo = new PDO($dsn);
+        $sTable = "reu_reuniones";
+        
+        if (isset($_GET['id'])){
+		   $id_reunion=intval($_GET['id']);
+		   $sql = "SELECT COUNT(*) AS cuantos FROM $sTable where idreunion=$id_reunion";
+           $stmt = $pdo->query($sql);
+           // Obtener el resultado (única fila)
+           $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+           // Número total de filas
+           $cuantos = $resultado['cuantos'];
+           if ($cuantos>0){
+               //valida que no haya reuniones asignadas a la dependencia
+               $sqlReu = "SELECT COUNT(*) AS cuantosReu FROM reu_reuniones_participante where idreunion=$id_reunion";
+               $stmtReu = $pdo->query($sqlReu);
+               $resReu  = $stmtReu->fetch(PDO::FETCH_ASSOC);
+               $cuantosReu = $resReu['cuantosReu'];
+               
+               if ($cuantosReu>0)
+               {
+        ?>           
+                    <div class="alert alert-danger alert-dismissible" role="alert">
+			            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			             <strong>Error!</strong> No se pudo eliminar ésta reunion. Existen registros vinculados a ésta reunión. 
+			        </div>
+		<?php	        
+               }
+               else
+               {
+                  //borrar reunión
+                  // Consulta SQL de eliminación con marcador de posición
+                  $sql = "DELETE FROM $sTable WHERE idreunion = :id_reunion";
+                  $stmt = $pdo->prepare($sql);
+                  // Vincular parámetro
+                  $stmt->bindParam(':id_reunion', $id_reunion, PDO::PARAM_INT);
+                  // Ejecutar la consulta
+                  $stmt->execute();
+                  // Verificar el número de filas afectadas (opcional)
+                  $borradas = $stmt->rowCount();
+                  if ($borradas>0){
+        ?>
+                      <div class="alert alert-success alert-dismissible" role="alert">
+			                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			                <strong>Atención!</strong> Datos eliminados exitosamente.
+			           </div>
+        <?php
+                  }
+                  else{
+        ?>              
+                     <div class="alert alert-danger alert-dismissible" role="alert">
+			           <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			           <strong>Error!</strong> Lo siento algo ha salido mal intenta nuevamente.
+			         </div> 
+		<?php	
+                  }
+                  
+               }
+               
+           }//cuantos
+           
+        }//get   
+        //aqui va ajax
+	    if($action == 'ajax'){
+	        // Limpiar y escapar la cadena de texto (strip_tags y htmlentities)
+            $q = strip_tags($_REQUEST['q']);
+            $q = htmlentities($q, ENT_QUOTES, 'UTF-8');
+            $q1 = strip_tags($_REQUEST['q']);
+            $q1 = htmlentities($q, ENT_QUOTES, 'UTF-8');
+            $q2 = strip_tags($_REQUEST['q']);
+            $q2 = htmlentities($q, ENT_QUOTES, 'UTF-8');
+            $q3 = strip_tags($_REQUEST['q']);
+            $q3 = htmlentities($q, ENT_QUOTES, 'UTF-8');
+            /* 
+             echo '<br>';
+             echo "q: " . $q;
+             echo '<br>';
+             echo "q1: " . $q1;
+             echo '<br>';
+             echo "q2: " . $q2;
+             echo '<br>';
+             echo "q3: " . $q3;
+             echo '<br>';
+             echo "q4: " . $q4;
+             echo '<br>';
+            */      
+            
+            $q = strtoupper($q);
+            $q1 = strtoupper($q1);
+            $q2 = strtoupper($q2);
+            $q3 = strtoupper($q3);
+            
+            $aColumns = array('idreunion');//Columnas de busqueda
+		    $sTable = "reu_reuniones";
+		    $sWhere = "";
 		 
 		    $sWhere = "WHERE (";
 			for ( $i=0 ; $i<count($aColumns) ; $i++ )
@@ -72,107 +112,127 @@
 			$sWhere .= ')';
 		
 		  	if ($q1>0){
-		        $sWhere .=" and convocadaPor ='$q1'";
+		        $sWhere .=" and convocadapor ='$q1'";
 		    }
 		    if ($q2>0){
-		        $sWhere .=" and idDependencia ='$q2'";
+		        $sWhere .=" and iddependencia ='$q2'";
 		    }
 		    if ($q3>0){
-		        $sWhere .=" and idGrupo ='$q3'";
+		        $sWhere .=" and idgrupo ='$q3'";
 		    }
-		 
-		$sWhere.=" order by idReunion";
-		include 'pagination.php'; //include pagination file
-		//pagination variables
-		$page = (isset($_REQUEST['page']) && !empty($_REQUEST['page']))?$_REQUEST['page']:1;
-		$per_page = 10; //how much records you want to show
-		$adjacents  = 4; //gap between pages after number of adjacents
-		$offset = ($page - 1) * $per_page;
-		//Count the total number of row in your table*/
-		$count_query   = mysqli_query($con, "SELECT count(*) AS numrows FROM $sTable  $sWhere");
-		$row= mysqli_fetch_array($count_query);
-		$numrows = $row['numrows'];
-		$total_pages = ceil($numrows/$per_page);
-		$reload = './marcas.php';
-		//main query to fetch the data
-		$sql="SELECT * FROM  $sTable $sWhere LIMIT $offset,$per_page";
-	    //echo "sql..." . $sql;
-		$query = mysqli_query($con, $sql);
-		//loop through fetched data
-		if ($numrows>0){
-			
-			?>
-			<div class="table-responsive">
-
-			  <table class='tablaResponsive table table-striped table-bordered table-hover'>
-				<tr  class="info">
-					<th>No.Reunion</th>
+           
+            
+		    // Configurar el modo de error para excepciones
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		    
+		    $sWhere.=" order by idreunion";
+		    include 'pagination.php'; //include pagination file
+		    //paginación variables
+		    $page = (isset($_REQUEST['page']) && !empty($_REQUEST['page']))?$_REQUEST['page']:1;
+		    $per_page = 10; //cuantos registros desea mostrar
+		    $adjacents  = 4; //gap entre paginas despues del número de adyacentes
+		    $offset = ($page - 1) * $per_page;
+		
+	        // Consulta SQL para contar las filas
+            $sql = "SELECT COUNT(*) AS total_filas FROM $sTable $sWhere";
+            $stmt = $pdo->query($sql);
+            // Obtener el resultado (única fila)
+            $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+            // Número total de filas
+            $total_filas = $resultado['total_filas'];
+            $total_pages = ceil($total_filas / $per_page);
+            $reload = './marcas.php';
+            //echo "El número total de filas en la tabla es: $total_filas";
+            if ($total_filas>0)
+            {
+        ?>        
+                <div class="table-responsive">
+			      <table class='tablaResponsive table table-striped table-bordered table-hover'>
+				    <th>No.Reunion</th>
 					<th>Fecha</th>
 					<th>Convocada por</th>
 					<th>Dependencia</th>
 					<!--<th>Grupo</th>-->
 					<th>Estado</th>
 					<th class='text-center' colspan="3">Acciones</th>
-				</tr>
-				<?php
-				while ($row=mysqli_fetch_array($query)){
-						$idReunion=$row['idReunion'];
-						$fechaReunion=$row['fechaReunion'];
-						$convocadaPor=$row['convocadaPor'];
-						$idDependencia=$row['idDependencia'];
-						$idGrupo=$row['idGrupo'];
-						$detalleReunion=$row['detalleReunion'];
-						$estadoReunion=$row['estadoReunion'];
+        <?php    
+                    $sql="SELECT * FROM  $sTable $sWhere OFFSET $offset LIMIT $per_page";
+                    
+                    $stmt = $pdo->query($sql);
+                    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                        $idReunion=$row['idreunion'];
+						$fechaReunion=$row['fechareunion'];
+						$convocadaPor=$row['convocadapor'];
+						$idDependencia=$row['iddependencia'];
+						$idGrupo=$row['idgrupo'];
+						$detalleReunion=$row['detallereunion'];
+						$estadoReunion=$row['estadoreunion'];
 						
-						$sqlPer   = "select * from reu_participante where numeroIdParticipante=$convocadaPor";
-						$queryPer = mysqli_query($con, $sqlPer);  
-						$rowPer   = mysqli_fetch_array($queryPer);
-			            $persona  = $rowPer['nombresParticipante'];  
-			            
-						$sqlDepen    = "select * from reu_dependencias where idDependencia=$idDependencia";
-						$queryDepen  = mysqli_query($con, $sqlDepen);  
-						$rowDepen    = mysqli_fetch_array($queryDepen);
-			            $depen       = $rowDepen['nombreDependencia'];  
+						//trae persona
+						$sqlPer   = "select * from reu_participante where numeroidparticipante=$convocadaPor";
+						$stmtPer = $pdo->query($sqlPer);
+					    $rowPer  = $stmtPer->fetch(PDO::FETCH_ASSOC);
+					    $persona     = $rowPer['nombresparticipante'];
 						
-						$sqlGrupo    = "select * from reu_grupos_internos where idGrupoInterno=$idGrupo";
-						$queryGrupo  = mysqli_query($con, $sqlGrupo);
-						$rowGrupo    = mysqli_fetch_array($queryGrupo);
-			            $grupo       = $rowGrupo['grupoInterno']; 
+			            //trae entidad
+					    $sqlEnt  ="select * from reu_entidades where identidad='$entidad'";
+					    // echo $sqlEnt;
+					    $stmtEnt = $pdo->query($sqlEnt);
+					    $rowEnt  = $stmtEnt->fetch(PDO::FETCH_ASSOC);
+					    $ent     = $rowEnt['nombreentidad'];
+					    
+					    //trae dependencia
+					    $sqlDep  ="select * from reu_dependencias where iddependencia=$dependencia";
+					    $stmtDep = $pdo->query($sqlDep);
+					    $rowDep  = $stmtDep->fetch(PDO::FETCH_ASSOC);
+    				    $depen     = $rowDep['nombredependencia'];
+    				    
+    				    //trae grupo
+					    $sqlGrupo  ="select * from reu_grupos_internos where idgrupoInterno=$idGrupo";
+					    $stmtGrupo = $pdo->query($sqlGrupo);
+					    $rowGrupo  = $stmtGrupo->fetch(PDO::FETCH_ASSOC);
+    				    $grupo  = $rowGrupo['grupoInterno']; 
  
  					    $lv   = $idReunion. "/MOD1234567890qwertyuiopasdfghjkl";
 					    $lVDX = base64_encode($lv);
-					?>
-					<tr>	
-  					   <td><?php echo $idReunion; ?></td>
-  					   <td><?php echo $fechaReunion; ?></td>
-  					   <td><?php echo $persona; ?></td>
-  					   <td><?php echo $depen; ?></td>
-  					   <!--<td><?php echo $grupo; ?></td>
-  					   <td><?php echo $detalleReunion; ?></td>-->
-  					   <td><?php echo $estadoReunion; ?></td>
+        ?>
+                        <tr>	
+  		   			      <td><?php echo $idReunion; ?></td>
+  					       <td><?php echo $fechaReunion; ?></td>
+  					       <td><?php echo $persona; ?></td>
+  					       <td><?php echo $depen; ?></td>
+  					       <!--<td><?php echo $grupo; ?></td>
+  					       <td><?php echo $detalleReunion; ?></td>-->
+  					       <td><?php echo $estadoReunion; ?></td>
  
-					   <td class='text-center'>
-					     <a href="reu1?LA=<?=$lVDX?>" class='btn btn-default' title='Editar reunión' ><i class="glyphicon glyphicon-edit"></i></a> 
-					   </td>  
-					   <td class='text-center'>  
-					     <a href="reuI.php?LA=<?=$lVDX?>" class='btn btn-default' title='Imprimir acta' ><i class="glyphicon glyphicon-print"></i></a> 
-					    </td>
-					    <td class='text-center'>  
-					     <a href="#" class='btn btn-default'title='Borrar reunión' onclick="eliminar('<?php echo $idReunion; ?>')"><i class="glyphicon glyphicon-trash"></i> </a>
-					    </td> 
-					</tr>
-					<?php
-				}
-				?>
-				<tr>
-					<td colspan="10"><span class="pull-right">
-					<?php
-					 echo paginate($reload, $page, $total_pages, $adjacents);
-					?></span></td>
-				</tr>
-			  </table>
-			</div>
-			<?php
-		}
-	}
-?>
+					       <td class='text-center'>
+					         <a href="reu1?LA=<?=$lVDX?>" class='btn btn-default' title='Editar reunión' ><i class="glyphicon glyphicon-edit"></i></a> 
+					       </td>  
+					       <td class='text-center'>  
+					         <a href="reuI.php?LA=<?=$lVDX?>" class='btn btn-default' title='Imprimir acta' ><i class="glyphicon glyphicon-print"></i></a> 
+					        </td>
+					        <td class='text-center'>  
+					         <a href="#" class='btn btn-default'title='Borrar reunión' onclick="eliminar('<?php echo $idReunion; ?>')"><i class="glyphicon glyphicon-trash"></i> </a>
+					        </td> 
+					   </tr>
+        <?php
+                    }//while
+        ?>
+                       <tr>
+					      <td colspan="2">
+					         <span class="pull-right">
+					            <?php
+					              echo paginate($reload, $page, $total_pages, $adjacents);
+					            ?>
+					          </span>
+					      </td>
+				       </tr>
+			     </table>
+        <?php
+            }// if>0
+	    }//ajax
+ 	}catch (PDOException $e) {
+    echo "Error de conexión: " . $e->getMessage();
+}    
+ 	
+ 	
