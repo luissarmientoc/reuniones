b6b49fbbc432a8d385a2f7db7f59462d5dc99129
Luis Sarmiento <luis@MacBook-Pro-de-Luis.local>
2024-07-23 15:12:52 -0500

    Version 1.3

--- dash.php
+++ dash.php
@@ -317,7 +317,7 @@
                       <div class="col-sm-12">
                         <div class="fondo" align="center">
                             <h3>Datos desde: <b><?=$s_fecIni?></b> Hasta: <b><?=$s_fecFin?></b></h3>
-                            <h5>Para consultar mas fechas toque la opción en el panel de Consultar por fehas de la parte superior <i class='fas fa-arrow-circle-up'></i></h5>
+                            <h5>Para consultar mas fechas toque la opción en el panel de Consultar por fehas de la parte superior <i class='fas fa-arrow-circle-up' style="font-size:18px;"></i></h5>
                         </div>             
                       </div>
                   </div>
