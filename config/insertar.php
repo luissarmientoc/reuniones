598d0749a904cba581e5cf0d0df66b9c47da7b02
Luis Sarmiento <luis@MacBook-Pro-de-Luis.local>
2024-07-25 11:42:37 -0500

    version 1.3

--- config/insertar.php
+++ config/insertar.php
@@ -23,11 +23,12 @@
     $s_tareaRealizada=13;
     $date_added=date("Y-m-d");
     $date_added="2024-01-01";
+    $terminada="S";
     
     $sql = "INSERT INTO reu_tareas_realizadas 
-                (idtarea, idreunion, numeroidparticipante, idcompromiso, tarearealizada, fechatarea) 
-                VALUES (?,?,?,?,?)";
-       $stmt->execute([$s_idTarea, $s_idReunion, $s_numeroIdParticipante, $s_idCompromiso, $s_tareaRealizada, $date_added ]);
+                (idtarea, idreunion, numeroidparticipante, idcompromiso, tarearealizada, fechatarea, terminada) 
+                VALUES (?,?,?,?,?,?)";
+       $stmt->execute([$s_idTarea, $s_idReunion, $s_numeroIdParticipante, $s_idCompromiso, $s_tareaRealizada, $date_added,  $terminada ]);
     
     echo '<br>';
     echo "Datos insertados correctamente.";
