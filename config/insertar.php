9d6865fae28003d72d02872506cc48bc27f24f30
Luis Sarmiento <luis@MacBook-Pro-de-Luis.local>
2024-07-25 11:36:19 -0500

    version 1.3

---
+++ config/insertar.php
@@ -0,0 +1,40 @@
+<?php
+ echo "algo";  echo "algo";  echo "algo";  echo "algo";  echo "algo";  echo "algo";  echo "algo"; 
+  $POSTGRESQL_HOST='172.16.20.121';
+  $POSTGRESQL_PORT='5432';
+  $POSTGRESQL_NAME='unp_reuniones';
+  $POSTGRESQL_USER='usr_reuniones';
+  $POSTGRESQL_PASS='Odisea1520sql';
+  
+  /*
+  $POSTGRESQL_NAME='robert';
+  $POSTGRESQL_USER='alban';
+  $POSTGRESQL_PASS='Iliada1520psql(sqrt(pi))';
+  */
+  
+  echo "algo"; 
+  
+  try {
+    
+    $s_idTarea=99;
+    $s_idReunion=100;
+    $s_numeroIdParticipante=79451305;
+    $s_idCompromiso=12;
+    $s_tareaRealizada=13;
+    $date_added=date("Y-m-d");
+    
+    $sql = "INSERT INTO reu_tareas_realizadas 
+                (idtarea, idreunion, numeroidparticipante, idcompromiso, tarearealizada, fechatarea) 
+                VALUES (?,?,?,?,?)";
+       $stmt->execute([$s_idTarea, $s_idReunion, $s_numeroIdParticipante, $s_idCompromiso, $s_tareaRealizada, $date_added ]);
+    
+    echo '<br>';
+    echo "Datos insertados correctamente.";
+    echo '<br>';
+    
+    
+} catch (PDOException $e) {
+    echo "Error de conexión: " . $e->getMessage();
+}
+
+?>  
