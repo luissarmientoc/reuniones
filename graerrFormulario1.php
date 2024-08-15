27c8d03fbeb12ff568834ea0ed878f7d5341d5c6
Luis Sarmiento <luis@MacBook-Pro-de-Luis.local>
2024-08-15 14:03:14 -0500

    version 1.3

--- graerrFormulario1.php
+++ graerrFormulario1.php
@@ -466,32 +466,12 @@ try {
     //$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // Preparar la consulta SQL
-    $sql = "INSERT INTO graerr_formulario (
+    $sql = "INSERT INTO graerr_formulario_a (
         id, registro, vigencia, fecha_recepcion_unp, fecha_recepcion_graerr, fecha_carta_solicitante,
-        no_mem_ext, otras_entradas_sigob, no_folios, entidad_persona_solicitante, destinatario,
-        tipo_documento, no_documento, nombres_peticionario, apellidos_peticionario, seudonimo,
-        tipo_ruta, descripcion_colectivo, nombre_colectivo, no_personas_evaluar, genero,
-        grupo_etnico, factor_diferencial, correo_electronico, no_de_contacto, otros_numeros_contacto,
-        direccion, departamento, municipio, corregimiento_vereda, autoriza_envio_info,
-        fecha_asignacion_analisis, analista_solicitudes, estado_solicitud, fecha_asignado_ot,
-        fecha_reasignacion_ot, medidas_preventivas, estado_ot, ot, analista_riesgo, analista_riesgo_dos,
-        analista_calidad, subpoblacion, tipo_estudio_riesgo, seguimiento, es_tramite_emergencia,
-        tramite_emergencia, fecha_tramite_emergencia, ingreso_calidad, fecha_aprobacion_calidad,
-        fecha_presentacion_premesa, recomendacion_riesgo_premesa, recomendacion_medidas_premesa,
-        observaciones_premesa, remision_mesa_tecnica, observaciones, otros
+        no_mem_ext, otras_entradas_sigob, no_folios, entidad_persona_solicitante, destinatario
     ) VALUES (
         DEFAULT, :registro, :vigencia, :fecha_recepcion_unp, :fecha_recepcion_graerr, :fecha_carta_solicitante,
-        :no_mem_ext, :otras_entradas_sigob, :no_folios, :entidad_persona_solicitante, :destinatario,
-        :tipo_documento, :no_documento, :nombres_peticionario, :apellidos_peticionario, :seudonimo,
-        :tipo_ruta, :descripcion_colectivo, :nombre_colectivo, :no_personas_evaluar, :genero,
-        :grupo_etnico, :factor_diferencial, :correo_electronico, :no_de_contacto, :otros_numeros_contacto,
-        :direccion, :departamento, :municipio, :corregimiento_vereda, :autoriza_envio_info,
-        :fecha_asignacion_analisis, :analista_solicitudes, :estado_solicitud, :fecha_asignado_ot,
-        :fecha_reasignacion_ot, :medidas_preventivas, :estado_ot, :ot, :analista_riesgo, :analista_riesgo_dos,
-        :analista_calidad, :subpoblacion, :tipo_estudio_riesgo, :seguimiento, :es_tramite_emergencia,
-        :tramite_emergencia, :fecha_tramite_emergencia, :ingreso_calidad, :fecha_aprobacion_calidad,
-        :fecha_presentacion_premesa, :recomendacion_riesgo_premesa, :recomendacion_medidas_premesa,
-        :observaciones_premesa, :remision_mesa_tecnica, :observaciones, :otros
+        :no_mem_ext, :otras_entradas_sigob, :no_folios, :entidad_persona_solicitante, :destinatario
     )";
     
     echo $sql;
@@ -510,52 +490,6 @@ try {
     $stmt->bindParam(':no_folios', $_POST['no_folios']);
     $stmt->bindParam(':entidad_persona_solicitante', $_POST['entidad_persona_solicitante']);
     $stmt->bindParam(':destinatario', $_POST['destinatario']);
-    $stmt->bindParam(':tipo_documento', $_POST['tipo_documento']);
-    $stmt->bindParam(':no_documento', $_POST['no_documento']);
-    $stmt->bindParam(':nombres_peticionario', $_POST['nombres_peticionario']);
-    $stmt->bindParam(':apellidos_peticionario', $_POST['apellidos_peticionario']);
-    $stmt->bindParam(':seudonimo', $_POST['seudonimo']);
-    $stmt->bindParam(':tipo_ruta', $_POST['tipo_ruta']);
-    $stmt->bindParam(':descripcion_colectivo', $_POST['descripcion_colectivo']);
-    $stmt->bindParam(':nombre_colectivo', $_POST['nombre_colectivo']);
-    $stmt->bindParam(':no_personas_evaluar', $_POST['no_personas_evaluar']);
-    $stmt->bindParam(':genero', $_POST['genero']);
-    $stmt->bindParam(':grupo_etnico', $_POST['grupo_etnico']);
-    $stmt->bindParam(':factor_diferencial', $_POST['factor_diferencial']);
-    $stmt->bindParam(':correo_electronico', $_POST['correo_electronico']);
-    $stmt->bindParam(':no_de_contacto', $_POST['no_de_contacto']);
-    $stmt->bindParam(':otros_numeros_contacto', $_POST['otros_numeros_contacto']);
-    $stmt->bindParam(':direccion', $_POST['direccion']);
-    $stmt->bindParam(':departamento', $_POST['departamento']);
-    $stmt->bindParam(':municipio', $_POST['municipio']);
-    $stmt->bindParam(':corregimiento_vereda', $_POST['corregimiento_vereda']);
-    $stmt->bindParam(':autoriza_envio_info', $_POST['autoriza_envio_info']);
-    $stmt->bindParam(':fecha_asignacion_analisis', $_POST['fecha_asignacion_analisis']);
-    $stmt->bindParam(':analista_solicitudes', $_POST['analista_solicitudes']);
-    $stmt->bindParam(':estado_solicitud', $_POST['estado_solicitud']);
-    $stmt->bindParam(':fecha_asignado_ot', $_POST['fecha_asignado_ot']);
-    $stmt->bindParam(':fecha_reasignacion_ot', $_POST['fecha_reasignacion_ot']);
-    $stmt->bindParam(':medidas_preventivas', $_POST['medidas_preventivas']);
-    $stmt->bindParam(':estado_ot', $_POST['estado_ot']);
-    $stmt->bindParam(':ot', $_POST['ot']);
-    $stmt->bindParam(':analista_riesgo', $_POST['analista_riesgo']);
-    $stmt->bindParam(':analista_riesgo_dos', $_POST['analista_riesgo_dos']);
-    $stmt->bindParam(':analista_calidad', $_POST['analista_calidad']);
-    $stmt->bindParam(':subpoblacion', $_POST['subpoblacion']);
-    $stmt->bindParam(':tipo_estudio_riesgo', $_POST['tipo_estudio_riesgo']);
-    $stmt->bindParam(':seguimiento', $_POST['seguimiento']);
-    $stmt->bindParam(':es_tramite_emergencia', $_POST['es_tramite_emergencia']);
-    $stmt->bindParam(':tramite_emergencia', $_POST['tramite_emergencia']);
-    $stmt->bindParam(':fecha_tramite_emergencia', $_POST['fecha_tramite_emergencia']);
-    $stmt->bindParam(':ingreso_calidad', $_POST['ingreso_calidad']);
-    $stmt->bindParam(':fecha_aprobacion_calidad', $_POST['fecha_aprobacion_calidad']);
-    $stmt->bindParam(':fecha_presentacion_premesa', $_POST['fecha_presentacion_premesa']);
-    $stmt->bindParam(':recomendacion_riesgo_premesa', $_POST['recomendacion_riesgo_premesa']);
-    $stmt->bindParam(':recomendacion_medidas_premesa', $_POST['recomendacion_medidas_premesa']);
-    $stmt->bindParam(':observaciones_premesa', $_POST['observaciones_premesa']);
-    $stmt->bindParam(':remision_mesa_tecnica', $_POST['remision_mesa_tecnica']);
-    $stmt->bindParam(':observaciones', $_POST['observaciones']);
-    $stmt->bindParam(':otros', $_POST['otros']);
 
     // Ejecutar la consulta
     $stmt->execute();
