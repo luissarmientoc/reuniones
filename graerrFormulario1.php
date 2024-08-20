fd911473f6277818676a6cee68aeb8dc2c1f6291
Luis Sarmiento <luis@MacBook-Pro-de-Luis.local>
2024-08-20 13:26:32 -0500

    Version 1.3

--- graerrFormulario1.php
+++ graerrFormulario1.php
@@ -942,10 +942,13 @@ try {
    
 
     //trae dependencia
+    if ($municipio>0)
+    {
 	$sqlDep  ="SELECT nommunicipio FROM reu_municipios where codmunicipio ='$municipio' and coddepto=$departamento";
 	$stmtDep = $pdo->query($sqlDep);
 	$rowDep  = $stmtDep->fetch(PDO::FETCH_ASSOC);
     $nommunicipio     = $rowDep['nommunicipio'];
+    }
   
    
   ?>  
