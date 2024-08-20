f11ddbd03c33f02207800a46ffb108defbec02ab
Luis Sarmiento <luis@MacBook-Pro-de-Luis.local>
2024-08-20 09:20:52 -0500

    Version 1.3

--- get_cities.php
+++ get_cities.php
@@ -6,6 +6,8 @@ require_once 'config/conexion.php'; // Incluye la función de conexión
 
 if (isset($_GET['department_id'])) {
     $department_id = intval($_GET['department_id']);
+    $sql="SELECT codmunicipio AS id, nommunicipio AS name FROM reu_municipios WHERE coddepto = ?";
+    echo  "en getcities..." . $sql;
     $stmt = $conn->prepare("SELECT codmunicipio AS id, nommunicipio AS name FROM reu_municipios WHERE coddepto = ?");
     $stmt->bind_param("i", $department_id);
     $stmt->execute();
