55e149b1fc4421975a787c83cc41210a8856d62b
Luis Sarmiento <luis@MacBook-Pro-de-Luis.local>
2024-08-26 12:55:49 -0500

    Version 1.3

--- graerrFormulario1.php
+++ graerrFormulario1.php
@@ -54,7 +54,7 @@
                       document.getElementById("nombre_colectivo").setAttribute("readonly", "readonly");
                       document.getElementById("nombres_peticionario").removeAttribute("readonly");
                       document.getElementById("apellidos_peticionario").removeAttribute("readonly");
-                      document.getElementById("seudonimo").setAttribute("readonly");
+                      document.getElementById("seudonimo").removeAttribute("readonly");
 
                      
                      miDiv1.style.display = 'none';
@@ -62,7 +62,7 @@
                  } else {
                       document.getElementById("nombres_peticionario").setAttribute("readonly", "readonly");
                       document.getElementById("apellidos_peticionario").setAttribute("readonly", "readonly");
-                      document.getElementById("seudonimo").removeAttribute("readonly", "readonly");
+                      document.getElementById("seudonimo").setAttribute("readonly", "readonly");
                       document.getElementById("descripcion_colectivo").removeAttribute("readonly");
                       document.getElementById("nombre_colectivo").removeAttribute("readonly");
                       
