4c851ff8431533b241edee383049068c6b7cbdba
Luis Sarmiento <luis@MacBook-Pro-de-Luis.local>
2024-08-30 07:40:18 -0500

    Version 1.3

--- graerrFormulario1.php
+++ graerrFormulario1.php
@@ -20,7 +20,7 @@
  <?php 
      require_once ("config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
      require_once ("config/conexion.php");//Contiene funcion que conecta a la base de datos
-     require("modal.php");
+    // require("modal.php");
      include("head.php");
   ?>
         
