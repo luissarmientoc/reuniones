11b9dc8776adc78c07f71c872bd053a41d74ada1
Luis Sarmiento <luis@MacBook-Pro-de-Luis.local>
2024-08-30 07:51:01 -0500

    Version 1.3

--- graerrFormulario1.php
+++ graerrFormulario1.php
@@ -1692,6 +1692,186 @@
                            -->
                        </div> <!--row-->
                        
+                       <div class="row" style="margin-top:5px;"> 
+                       <hr>
+                            <div class="col-sm-6">
+                               <div class="form-group">
+                                    <label class="labelDireccion" for="addressType">Tipo de dirección:</label>
+                                    <select class="form-control" id="addressType" required>
+                                        <option value="" disabled selected>Selecciona una opción</option>
+                                        <option value="">Seleccione una opción</option>
+                                        <option value="rural">Rural</option>
+                                        <option value="urbano">Urbano</option>
+                                    </select>
+                                </div>
+                            </div>
+                            
+                            <div class="col-sm-6">
+                                <div class="form-group" id="ruralOptions" style="display: none;">
+                                     <label class="labelDireccion" for="ruralType">Tipo de rural:</label>
+                                     <select class="form-control" id="ruralType">
+                                         <option value="" disabled selected>Selecciona un tipo</option>
+                                         <option value="corregimiento">Corregimiento</option>
+                                         <option value="centro_poblado">Centro Poblado</option>
+                                         <option value="vereda">Vereda</option>
+                                         <option value="otro">Otro</option>
+                                     </select>
+                                </div>
+                             
+                                <div class="form-group" id="urbanoOptions" style="display: none;">
+                                    <label class="labelDireccion" for="urbanoType">Tipo de urbano:</label>
+                                    <select class="form-control" id="urbanoType">
+                                        <option value="" disabled selected>Selecciona un tipo</option>
+                                        <option value="tipo_via">Tipo de Vía</option>
+                                        <option value="barrio">Barrio</option>
+                                        <option value="campo_abierto">Campo Abierto</option>
+                                    </select>
+                                </div>
+                            </div> 
+                       </div> <!--row-->
+                       
+                       <div class="row" style="margin-top:5px;"> 
+                            <div class="col-sm-2">
+                                <div class="form-group" id="tipo_via">
+                                    <label class="labelDireccion" for="tipo_via">Tipo de vía:</label>
+                                    <div class="form-group">
+                                        <select <?=$active?> required class="form-control" id="tipo_via">
+                                            <?=$combo_tipo_via?>
+                                        </select>
+                                    </div> 
+                                </div>   
+                            </div>
+                            
+                            <div class="col-sm-2">
+                                <div class="form-group" id="cuadrante_tipo_via">
+                                   <label class="labelDireccion" for="cuadrante_tipo_via">Cuadrante:</label>
+                                   <div class="form-group">
+                                     <select <?=$active?> required class="form-control" id="cuadrante">
+                                         <?php echo $combo_cuadrante; ?>
+                                      </select>
+                                   </div> 
+                                </div>  
+                            </div>
+                       
+                            <div class="col-sm-2">
+                                <div class="form-group" id="via_generadora">
+                                   <label class="labelDireccion" class="labelDireccion" for="via_generadora">No. inical placa:</label>
+                                   <input type="number" class="form-control" id="via_generadora" name="via_generadora" min="0" place holder="Vía Generadora">
+                                </div>  
+                            </div>
+                            
+                            <div class="col-sm-2">
+                                <div class="form-group" id="letra_via_generadora">
+                                   <label class="labelDireccion" for="letra_via_generadora">Letra:</label>
+                                   <select class="form-control" id="letra_via_generadora" name="letra_via_generadora">
+                                       <!-- Opciones del A a la Z -->
+                                       <option value="">Seleccione una letra</option>
+                                        <option value="A">A</option>
+                                       <option value="B">B</option>
+                                       <option value="C">C</option>
+                                       <option value="D">D</option>
+                                       <option value="E">E</option>
+                                       <option value="F">F</option>
+                                       <option value="G">G</option>
+                                       <option value="H">H</option>
+                                       <option value="I">I</option>
+                                       <option value="J">J</option>
+                                       <option value="K">K</option>
+                                       <option value="L">L</option>
+                                       <option value="M">M</option>
+                                       <option value="N">N</option>
+                                       <option value="O">O</option>
+                                       <option value="P">P</option>
+                                       <option value="Q">Q</option>
+                                       <option value="R">R</option>
+                                       <option value="S">S</option>
+                                       <option value="T">T</option>
+                                       <option value="U">U</option>
+                                       <option value="V">V</option>
+                                       <option value="W">W</option>
+                                       <option value="X">X</option>
+                                       <option value="Y">Y</option>
+                                       <option value="Z">Z</option>
+                                     </select>
+                                   </div>  
+                                </div> 
+                                
+                                <div class="col-sm-2">
+                                    <div class="form-group" id="sufijo">
+                                       <label class="labelDireccion" for="via_generadora">Sufijo:</label>
+                                       <select class="form-control" id="sufijo" name="sufijo">
+                                          <!-- Opciones del A a la Z -->
+                                          <option value="">Seleccione el Sufijo</option>
+                                          <option value="Bis">Bis</option>
+                                        </select>          
+                                    </div>  
+                                </div>
+                                
+                                <div class="col-sm-2">
+                                <div class="form-group" id="letra_sufijo">
+                                   <label class="labelDireccion" for="letra_sufijo">Letra:</label>
+                                   <select class="form-control" id="letra_sufijo" name="letra_sufijo">
+                                       <!-- Opciones del A a la Z -->
+                                       <option value="">Seleccione una letra</option>
+                                        <option value="A">A</option>
+                                       <option value="B">B</option>
+                                       <option value="C">C</option>
+                                       <option value="D">D</option>
+                                       <option value="E">E</option>
+                                       <option value="F">F</option>
+                                       <option value="G">G</option>
+                                       <option value="H">H</option>
+                                       <option value="I">I</option>
+                                       <option value="J">J</option>
+                                       <option value="K">K</option>
+                                       <option value="L">L</option>
+                                       <option value="M">M</option>
+                                       <option value="N">N</option>
+                                       <option value="O">O</option>
+                                       <option value="P">P</option>
+                                       <option value="Q">Q</option>
+                                       <option value="R">R</option>
+                                       <option value="S">S</option>
+                                       <option value="T">T</option>
+                                       <option value="U">U</option>
+                                       <option value="V">V</option>
+                                       <option value="W">W</option>
+                                       <option value="X">X</option>
+                                       <option value="Y">Y</option>
+                                       <option value="Z">Z</option>
+                                     </select>
+                                   </div>  
+                                </div> 
+                       </div> <!--row-->
+                       
+                       <div class="row" style="margin-top:5px;"> 
+                                <div class="col-sm-2">
+                                    <div class="form-group" id="numero_placa">
+                                        <label class="labelDireccion" for="numero_placa">Número de placa:</label>
+                                         <input type="number" class="form-control" id="numero_placa" name="numero_placa" min="0">
+                                    </div>  
+                                </div>
+                                 
+                                <div class="col-sm-2">
+                                    <div class="form-group" id="cuadrante_numero_placa">
+                                      <label class="labelDireccion" for="cuadrante_numero_placa">Cuadrante:</label>
+                                      <div class="form-group">
+                                          <select <?=$active?> required class="form-control" id="cuadrante_numero_placa">
+                                            <?php echo $combo_cuadrante; ?>
+                                          </select>
+                                      </div>
+                                    </div>  
+                                </div>
+                                
+                                <div class="col-sm-8">
+                                    <div class="form-group" id="complemento">
+                                      <label class="labelDireccion" for="complemento">Complemento:</label>
+                                      <textarea  style="text-transform:uppercase;" class="form-control" id="complemento" name="complemento" rows="1">  </textarea>
+                                    </div>  
+                                </div>
+                         <hr>
+                       </div> <!--row-->
+                       
                        <div class="row" style="margin-top:5px;">  
                           <div class="col-sm-6" align="left">  
                               <label for="direccion">DIRECCION</label>
@@ -1704,6 +1884,7 @@
                           </div>
                           
                        </div> <!--row-->
+                       
                        
                     </div> <!--container-->
                     
