816cbbe64d5cccab5e69946188720701b9dde937
Luis Sarmiento <luis@MacBook-Pro-de-Luis.local>
2024-09-09 12:25:09 -0500

    Version 1.3

--- graerrFormulario1.php
+++ graerrFormulario1.php
@@ -1339,7 +1339,7 @@
                  $stmt1 = $pdo->prepare('INSERT INTO graerr_direccion (
                                     registro, addresstype, ruraltype, urbanotype, tipo_via,cuadrante, num_via_principal, letra_via_principal, prefijo_bis_via_principal, via_generadora, letra_via_generadora, 
                                     sufijo, letra_sufijo, numero_placa, cuadrante_numero_placa, complemento
-                                    ) VALUES (?, ?, 
+                                    ) VALUES (?, ?, ?,?,?,
                                               ?, ?, 
                                               ?, ?, 
                                               ?, ?,
